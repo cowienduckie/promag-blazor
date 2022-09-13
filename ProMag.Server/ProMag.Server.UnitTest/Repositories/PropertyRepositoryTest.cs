def5844775c507c804b51e6e9e0c412c1747db60
Minh Tran <minhtran.2404@outlook.com>
2022-09-13 16:04:02 +0700

    Update Unit tests

---
+++ ProMag.Server/ProMag.Server.UnitTest/Repositories/PropertyRepositoryTest.cs
@@ -0,0 +1,74 @@
+﻿using ProMag.Server.Core.Domain.Entities;
+using Xunit;
+using FluentAssertions;
+
+namespace ProMag.Server.UnitTest.Repositories;
+
+public class PropertyRepositoryTest : BaseRepositoryTest<Property>
+{
+    [Fact]
+    public async Task GetAllProperties()
+    {
+        // Arrange
+        var propertyType = new PropertyType
+        {
+            Id = 1,
+            Name = "Testing"
+        };
+        var property1 = new Property
+        {
+            Id = 12,
+            Name = "Property1",
+            TypeId = 1,
+            Value = "Value1"
+        };
+        var property2 = new Property
+        {
+            Id = 123,
+            Name = "Property2",
+            TypeId = 1,
+            Value = "Value2"
+        };
+
+        _context.PropertyTypes.Add(propertyType);
+        _context.Properties.Add(property1);
+        _context.Properties.Add(property2);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var properties = await _repository.GetAllAsync();
+
+        // Assert
+        properties.Count().Should().Be(2);
+        properties.Should().Contain(x => x.Id == 12);
+        properties.Should().Contain(x => x.Id == 123);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task GetPropertyById(int propertyId)
+    {
+        // Arrange
+        const int typeId = 1;
+
+        _context.PropertyTypes.Add(new PropertyType
+        {
+            Id = typeId,
+            Name = "Testing"
+        });
+        _context.Properties.Add(new Property
+        {
+            Id = propertyId,
+            Name = "Property",
+            TypeId = typeId,
+            Value = "Value"
+        });
+        await _context.SaveChangesAsync();
+
+        // Act
+        var property = await _repository.GetByIdAsync(propertyId);
+
+        // Assert
+        property?.Id.Should().Be(propertyId);
+    }
+}
---
+++ ProMag.Server/ProMag.Server.UnitTest/Repositories/SubTaskRepositoryTest.cs
@@ -0,0 +1,58 @@
+﻿using ProMag.Server.Core.Domain.Entities;
+using Xunit;
+using FluentAssertions;
+
+namespace ProMag.Server.UnitTest.Repositories;
+
+public class SubTaskRepositoryTest : BaseRepositoryTest<SubTask>
+{
+    [Fact]
+    public async Task GetAllSubTasks()
+    {
+        // Arrange
+        var subTask1 = new SubTask
+        {
+            Id = 12,
+            Name = "SmartLiving",
+            Description = "A smart home subTask"
+        };
+        var subTask2 = new SubTask
+        {
+            Id = 123,
+            Name = "ProMag",
+            Description = "A subTask management subTask",
+        };
+
+        _context.SubTasks?.Add(subTask1);
+        _context.SubTasks?.Add(subTask2);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var subTasks = await _repository.GetAllAsync();
+
+        // Assert
+        subTasks.Count().Should().Be(2);
+        subTasks.Should().Contain(x => x.Id == 12);
+        subTasks.Should().Contain(x => x.Id == 123);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task GetSubTaskById(int subTaskId)
+    {
+        // Arrange
+        _context.SubTasks?.Add(new SubTask
+        {
+            Id = subTaskId,
+            Name = "SubTask",
+            Description = "SubTask Description",
+        });
+        await _context.SaveChangesAsync();
+
+        // Act
+        var subTask = await _repository.GetByIdAsync(subTaskId);
+
+        // Assert
+        subTask?.Id.Should().Be(subTaskId);
+    }
+}
